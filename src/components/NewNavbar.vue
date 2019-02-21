daa411c48c1855836328d38cf356f534a9ab7d89
Guilherme Oliveira <guilherme.061@gmail.com>
2019-02-21 11:31:14 -0300

    adding fact sheet to navbar

--- src/components/NewNavbar.vue
+++ src/components/NewNavbar.vue
@@ -7,6 +7,9 @@
                         <a href="#freshmansPress" class="nav-link">PRESS KIT</a>
                     </li>
                     <li class="nav-item">
+                        <a href="#freshmansFactSheet" class="nav-link">FACT SHEET</a>
+                    </li>
+                    <li class="nav-item">
                         <a href="#freshmansContact" class="nav-link">CONTACT US</a>
                     </li>
                 </ul>
